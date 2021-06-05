6f41f07bdf0a9e1fc21bf48bd8d1db261b5f3d11
IMightBeMe <>
2021-06-05 21:34:02 +0000

    Added a Favicon and updated the feedback to your answer.

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-// const favicon = require('serve-favicon');
 
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(
-//   favicon(path.join(__dirname, 'public', 'img', 'favicon', 'favicon.ico'))
-// );
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
+
+app.get('/favicon.ico', (req, res) => {
+	res.sendFile(`${__dirname}/public/img/favicon/favicon.ico`)
+})
 
 app.listen(8080, () => console.log('started server'));
--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,34 @@
 const start = () => {
-  const api = fetch('https://random-words-api.vercel.app/word');
+	const api = fetch('https://random-words-api.vercel.app/word');
 
-  api
-    .then((response) => {
-      return response.json();
-    })
-    .then((def) => {
-      const definition = def['0']['definition'];
-      const definitionText = document.getElementById('def');
-      const submit = document.getElementById('submit');
+	api
+		.then((response) => {
+			return response.json();
+		})
+		.then((def) => {
+			const definition = def['0']['definition'];
+			const definitionText = document.getElementById('def');
+			const submit = document.getElementById('submit');
+			const userGuess = document.getElementById('userGuess');
+			const yesNo = document.getElementById('yes-no');
 
-      console.log(def['0']['word']); // word
-      console.log(def['0']['definition']); // definition
+			console.log(def['0']['word']);
+			console.log(def['0']['definition']);
 
-      definitionText.innerHTML = `Definition: ${definition}`;
+			definitionText.innerHTML = `Definition: ${definition}`;
 
-      submit.onclick = () => {
-        if (document.getElementById('userGuess').value == def['0']['word']) {
-          console.log('right answer');
-          start();
-        } else {
-          console.log('wrong');
-          start();
-        }
-      };
-    });
+			feedback(def, userGuess, submit, yesNo);
+		});
+};
+
+const feedback = (def, userGuess, submit, yesNo) => {
+	submit.onclick = () => {
+		if (userGuess.value == def['0']['word']) {
+			yesNo.innerHTML = `<p class='feedback'>Correct! The word was ${def['0']['word']}</p>`;
+			start();
+		} else {
+			yesNo.innerHTML = `<p class='feedback'>Wrong! The word was ${def['0']['word']}</p>`;
+			start();
+		}
+	};
 };
